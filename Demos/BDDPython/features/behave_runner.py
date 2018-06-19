be212d36e9cd6ef790bc8ffd48fceea2cd10c2b1
RealityCtrl <david.mccandless@outlook.com>
2018-06-19 19:55:54 +0100

    updating runner to run all feature files

--- Demos/BDDPython/features/behave_runner.py
+++ Demos/BDDPython/features/behave_runner.py
@@ -1,3 +1,11 @@
+def getfiles():
+    import glob
+
+    mylist = [f for f in glob.glob("*.feature")]
+    return mylist
+
+
 if __name__ == '__main__':
     from behave import __main__ as behave_executable
-    behave_executable.main('gamereview.feature')
+    features = getfiles()
+    behave_executable.main(features)
